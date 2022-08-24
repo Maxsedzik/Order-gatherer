e3c066e74346731f3481c44c684796f0d45c9143
Maxsedzik <maksymilian@maxsedzik.com>
2022-08-24 23:16:54 +0200

    Added collecting data from form

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,13 +3,36 @@ import { createContext, useState } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-   const [order,setOrder] = useState([]);
-   
-   function addNewOrder = ()=>{
+  const [ordName, setOrdName] = useState("");
+  const [ordText, setOrdText] = useState("");
+  const [ordTypeDark, setOrdTypeDark] = useState(false);
+  const [newOrder, setNewOrder] = useState(false);
 
-   }
+  const changeName = (e) => {
+    setOrdName(e.target.value);
+  };
 
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const changeText = (e) => {
+    setOrdText(e.target.value);
+  };
+
+  const setDark = () => {
+    setOrdTypeDark(!ordTypeDark);
+  };
+
+  const submitOrder = (e) => {
+    e.preventDeafault();
+    setNewOrder(true);
+    console.log(newOrder);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ changeName, changeText, setDark, submitOrder }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export default AppContext;
